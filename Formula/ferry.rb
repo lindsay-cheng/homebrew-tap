98d0d59d87077d25a86f6b27896e892fdbd9bce9
lindsay-cheng <lindsaycheng2@gmail.com>
2026-09-22 23:15:38 -0400

    Point ferry v0.1.1 at the latest tag
    Checksum matches the re-tagged v0.1.1 archive on GitHub.

--- Formula/ferry.rb
+++ Formula/ferry.rb
@@ -2,7 +2,7 @@ class Ferry < Formula
   desc "Move a Claude Code session from one computer to another"
   homepage "https://github.com/lindsay-cheng/ferry"
   url "https://github.com/lindsay-cheng/ferry/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "19c795aedd91d81547005b3c1b2f1b30e51523233abe2d5eda44b2204c2adbb7"
+  sha256 "4bc8c82b5813f3becb1a688d71c43e0f3975c48028b33d63275f52c64e49709d"
   license "MIT"
 
   depends_on "go" => :build
